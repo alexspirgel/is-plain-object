940e4762a706160de9a7cd849f45913e9c118f8c
Alex Spirgel <spirgel.alexander@gmail.com>
2020-12-03 11:03:38 -0500

    Fixed comparison logic error

--- isPlainObject.js
+++ isPlainObject.js
@@ -1,16 +1,16 @@
 /**
- * isPlainObject v1.0.0
+ * isPlainObject v1.0.1
  * https://github.com/alexspirgel/isPlainObject
  */
 
 const isPlainObject = (object) => {
-	if (!Object.prototype.toString.call(object) === '[object Object]') {
+	if (Object.prototype.toString.call(object) !== '[object Object]') {
 		return false;
 	}
   if (object.constructor === undefined) {
 		return true;
 	}
-  if (!Object.prototype.toString.call(object.constructor.prototype) === '[object Object]') {
+  if (Object.prototype.toString.call(object.constructor.prototype) !== '[object Object]') {
 		return false;
 	}
   if (!object.constructor.prototype.hasOwnProperty('isPrototypeOf')) {
