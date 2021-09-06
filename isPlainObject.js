3ac8974229cc389707ddf397f3d6f3cb158dc946
Alex Spirgel <spirgel.alexander@gmail.com>
2021-09-06 15:50:56 -0400

    Minor formatting update

--- isPlainObject.js
+++ isPlainObject.js
@@ -1,24 +1,25 @@
-/**
- * isPlainObject v1.0.1
- * https://github.com/alexspirgel/isPlainObject
- */
+/*
+isPlainObject v1.0.1
+https://github.com/alexspirgel/isPlainObject
+*/
 
 const isPlainObject = (object) => {
 	if (Object.prototype.toString.call(object) !== '[object Object]') {
 		return false;
 	}
-  if (object.constructor === undefined) {
+	if (object.constructor === undefined) {
 		return true;
 	}
-  if (Object.prototype.toString.call(object.constructor.prototype) !== '[object Object]') {
+	if (Object.prototype.toString.call(object.constructor.prototype) !== '[object Object]') {
 		return false;
 	}
-  if (!object.constructor.prototype.hasOwnProperty('isPrototypeOf')) {
-    return false;
-  }
-  return true;
+	if (!object.constructor.prototype.hasOwnProperty('isPrototypeOf')) {
+		return false;
+	}
+	return true;
 };
 
-if (typeof module !== 'undefined' && module.exports) {
+try {
 	module.exports = isPlainObject;
 }
+catch (e) {}
